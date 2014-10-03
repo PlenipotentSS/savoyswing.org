15451e79c40ed1e3e321cab79510454da77cd838
plenipotentSS <stevensonhoyt@icloud.com>
2014-10-03 01:36:44 -0700

    added template files for splash page

--- index.php
+++ index.php
@@ -3,11 +3,45 @@
 	<link rel="stylesheet" href="public/application.css" type="text/css" media="all">
 </head>
 <body>
+	<div class="pixel-cover"></div>
 	<div class="body-wrapper">
 		<header>
-			HEADER!
+			<h2>Savoy Swing Club</h2>
 		</header>
-		OK
-	</body>
+		<main>
+			<article class="content">
+				<section>
+					<h3>Looking for Savoy Mondays?</h3>
+					<p>
+						Savoy Swing Club and Savoy Mondays have been a long standing tradition. We apologize for our absence, but we are taking some big steps to make everyone's experience better.
+						Our aims are always to the community, and we are excited to take these steps to move forward.
+					</p>
+					<p>
+						Please note all Savoy Mondays classes will be on hold until early 2015.
+					</p>
+					<p>
+						Join us in the countdown to our new and improved site.
+				</section>
+			</article>
+			<article class="foot-1">
+					We apologize for any inconvenience, but we look forward to having a new site for you soon. We are also looking at revamping our classes for 2015, and we ask for your patience until then.
+					<br />
+					<br />
+					~Savoy Swing Club Board of Directors
+			</article>
+			<article class="foot-2">
+					<span id="day-count" class="day-counter">30</span><span class="day-metric">Days Till <br /> Launch</span>
+			</article>
+		</main>
+		<footer>
+			<ul>
+				<li>Savoy Swing Club, a 501(c)3 Organization</li>
+				<li><a href="mailto:hello@savoyswing.org">hello@savoyswing.org</a></li>
+				<li><a href="http://twitter.com/savoyswing">@savoyswing</a></li>
+			</ul>
+		</footer>
+	</div>
+	<script type="text/javascript" src="public/js/main.js"></script>
+</body>
 </body>
 </html>
